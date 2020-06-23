7b27ef9848640452dec476352946dd3f2345da15
Gondlir <kaoeskt@hotmail.com>
2020-06-23 01:38:00 -0300

    Enemy Behaviour

---
+++ plataform-project/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    [SerializeField] private float velocity;
+    [SerializeField] private Transform target;
+
+    private MovimentController movimentC;
+    private void Start()
+    {
+        movimentC = GetComponent<MovimentController>();
+    }
+    private void Update()
+    {
+        FollowThePlayer();
+    }
+
+    void FollowThePlayer()
+    {
+        float followPos = (target.position.x > this.transform.position.x) ? velocity : -velocity;
+        movimentC.Move(followPos);
+    }
+}
---
+++ plataform-project/Assets/Scripts/MovimentController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
+public class MovimentController : MonoBehaviour
+{
+    new private Collider2D collider;
+    private Rigidbody2D rb;
+
+    private bool lookBackwards;
+    private void Awake()
+    {
+        collider = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+ 
+
+    public void Move(float velocity)
+    {
+        rb.velocity = new Vector2(velocity, rb.velocity.y);
+        if (velocity < 0 && !lookBackwards)
+            Flip(true);
+        else if (velocity > 0 && lookBackwards)
+            Flip(false);
+    }  
+    public void Flip(bool turnLeft)
+    {
+        lookBackwards = !lookBackwards;
+        transform.localScale = new Vector3(turnLeft ? 1 : -1, 1, 1);
+    }
+}
